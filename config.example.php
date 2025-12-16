f299b523ed52af69f526d1a0fe4eeab8980032c0
Juan Bastidas <jbastidas@digitalmindtec.com>
2025-12-16 18:58:41 -0500

    Se agrego el gitignore

---
+++ config.example.php
@@ -0,0 +1,22 @@
+<?php
+//BASE DE DATOS
+define('servidor','localhost');
+define('db','database_name');
+define('usuario','root');
+define('contrasena','');
+//FUNCIONALIDAD DEL SISTEMA
+define('name_session','ADMIN_MI_COMMERCE_WEB');
+define('DURACION_SESION','7200'); //2 horas
+
+define('url_sistema','https://digitalmindtec.com/');
+define('url_upload','/home1/digitalmind/public_html/');
+define('url_pages_installers','dashboard.mie-commerce.com/replicador/zip/');
+
+// define('url_sistema','https://dashboard.mie-commerce.com/');
+// define('url_upload',$_SERVER['DOCUMENT_ROOT'].'/');
+
+define('url_bot','https://dashboard.mie-commerce.com/bot/');
+define('url_folder_demo','/home1/digitalmind/demo.mie-commerce.com/');
+define('firebaseMessagingToken','firebasetoken');
+define('ENVIRONMENT','development'); //development  or production
+define('url_laar','https://api.laarcourier.com:9727/');
